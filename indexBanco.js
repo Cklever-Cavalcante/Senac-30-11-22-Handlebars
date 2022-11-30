8c5177a9d5a1099ba0b05530140e80ad130969fa
Cklever-Cavalcante <95925101+Cklever-Cavalcante@users.noreply.github.com>
2022-11-30 16:28:59 -0300

    Handlebars

---
+++ indexBanco.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const { engine } = require("express-handlebars");
+const bodyParser = require("body-parser");
+const Post = require("./models/Post");
+const Sequelize = require("sequelize");
+
+
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/", function (req, res) {
+    res.render("home")
+});
+
+app.get("/cad", function (req, res) {
+    res.render("formulario")
+});
+
+app.post("/add", function (req, res) {
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(function () {
+        //res.send("Post criado com sucesso")
+        res.redirect("/")
+    }).catch(function (erro) {
+        res.send("Houve um erro:" + erro)
+    })
+});
+
+app.listen(8090, function () {
+    console.log(" Servidor rodando no localhost:8090 ");
+});
